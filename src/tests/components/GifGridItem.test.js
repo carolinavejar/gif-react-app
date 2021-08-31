4bf7323a93b51507238a45a230ebfc90ad6ddec3
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-08-31 12:39:18 -0400

    Clase 90 Pruebas componente GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,9 +5,31 @@ import { GifGridItem } from '../../components/GifGridItem';
 describe("Pruebas de Gif Grid Item", ()=> {
     const title = `Titulo`;
     const url = `https://localhost:3000`;
+    const wrapper = shallow(<GifGridItem title = {title}  url = {url} />);
+
     test('debe de mostrar <GifGridItem /> correctamente', () => {
-        const wrapper = shallow(<GifGridItem title = {title}  url = {url} />);
         expect (wrapper).toMatchSnapshot();
-    })    ;
+    });
+
+    test('debe tener parrafo con un title', () => {
+        const p = wrapper.find('.card-title');
+        expect (p.text().trim()).toBe(title);
+    });
+
+    test('debe tener imagen igual a la url y alt de props ', () => {
+        const img = wrapper.find('img');
+        expect (img.prop('src')).toBe(url);
+        expect (img.prop('alt')).toBe(title);
+    });
+
+    test('debe tener la clase animate__bounce', () => {
+        const div = wrapper.find('div.animate__bounce').exists();
+        expect (div).toBe(true);
+
+        // const div = wrapper.find('div').exists();
+        // const className = div.prop('className');
+        // expect (className.includes('animate__bounce')).toBe(true);
+        // ASI LO REALIZÓ EL PROFESOR
+    });
 
 })
