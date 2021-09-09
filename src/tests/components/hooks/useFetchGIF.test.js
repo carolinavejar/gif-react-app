03cd2d1ae6b08ecd561c4d8510f20dfd9f27c6a1
Carolina Vejar <vejarcarolina.95@gmail.com>
2021-09-08 22:48:36 -0300

    Clase 100 customHook - waitForNextUpdate

--- src/tests/components/hooks/useFetchGIF.test.js
+++ src/tests/components/hooks/useFetchGIF.test.js
@@ -2,11 +2,20 @@ import { useFetchGIF } from '../../../components/hooks/useFetchGIF';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas en hook', () =>{
-    test('debe retornar el estado inicial ', () => {
-        const { result } = renderHook(()=> useFetchGIF('Rihanna'));
+    test('debe retornar el estado inicial ', async () => {
+        const { result, waitForNextUpdate } = renderHook(()=> useFetchGIF('Rihanna'));
         const { data, loading } = result.current;
+        await waitForNextUpdate();
+
         expect(data).toEqual([]);
         expect(loading).toBe(true);
-    })
-    
+    });
+
+    test('debe retornar un array de imgs y loading en false ', async() => {
+        const { result, waitForNextUpdate } = renderHook(()=> useFetchGIF('Rihanna'));
+        await waitForNextUpdate();
+        const { data, loading } = result.current;
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
+    });
 }) 
